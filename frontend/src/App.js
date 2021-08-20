a33de797087c16de47ae6bf06641aa7cd4ed2f4d
fad16papa <fad16papa@gmail.com>
2021-08-20 17:45:16 +0800

    Commit changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,6 @@ import "./assets/scss/material-kit-pro-react.scss?v=1.9.0";
 import LoginPage from "./views/LoginPage/LoginPage.js";
 import RegisterPage from "./views/RegisterPage/RegisterPage";
 import HomePage from "./views/HomePage/HomePage";
-import HeaderMain from "./components/Header/HeaderMain";
 import ProductPage from "./views/ProductPage/ProductPage";
 import CartPage from "./views/CartPage/CartPage";
 
@@ -15,12 +14,12 @@ const App = () => {
   return (
     <Router>
       <Fragment>
-        <Route path='/' component={HomePage} exact />
-        <Route path='/login' component={LoginPage} />
-        <Route path='/register' component={RegisterPage} />
-        <Route path='/product/:id' component={ProductPage} />
-        <Route path='/cart/:id' component={CartPage} exact />
-        <Route path='/cart' component={CartPage} exact />
+        <Route path="/" component={HomePage} exact />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/register" component={RegisterPage} />
+        <Route path="/product/:id" component={ProductPage} />
+        <Route path="/cart/:id" component={CartPage} exact />
+        <Route path="/cart" component={CartPage} exact />
       </Fragment>
     </Router>
   );
